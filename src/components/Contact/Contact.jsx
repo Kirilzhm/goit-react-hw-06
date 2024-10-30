da367fb7d86b504b7e8917ef3f4d7298d7d8d401
Kyrylo <kirilzhm@gmail.com>
2024-10-30 23:43:29 +0100

    changed small errors

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactsSlice';
 
-const Contact = ({ name, number }) => (
-        <div>
-    <p>👤 { name }</p>
-    <p>📞 {number}</p>
-        </div>
-);
+const Contact = ({ id, name, number }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteContact(id));
+  };
+
+  return (
+    <div>
+      <p>👤 {name}</p>
+      <p>📞 {number}</p>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
+};
 
 export default Contact;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,15 @@
-import { useDispatch } from 'react-redux';
-import { deleteContact } from '../../redux/contactsSlice';
+import { useSelector } from 'react-redux';
+import { selectFilteredContacts } from '../../redux/contactsSlice';
 import Contact from '../Contact/Contact';
 
-const ContactList = ({ contacts }) => {
-  const dispatch = useDispatch();
-  console.log(contacts);
-  const handleDelete = (id) => {
-    dispatch(deleteContact(id));
-  };
+const ContactList = () => {
+  const contacts = useSelector(selectFilteredContacts);
 
   return (
     <ul>
       {contacts.map((contact) => (
         <li key={contact.id}>
-          <Contact name={contact.name} number={contact.number} />
-          <button onClick={() => handleDelete(contact.id)}>Delete</button>
+         <Contact id={contact.id} name={contact.name} number={contact.number} />
         </li>
       ))}
     </ul>
